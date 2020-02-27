09408608c58efc7e8474bade69236b2c549b55e1
bishabosha <bishbashboshjt@gmail.com>
2020-02-27 21:49:39 +0100

    change to Dotty 0.21.0, update readme, simplify test runner

--- project/DottySupport.scala
+++ project/DottySupport.scala
@@ -14,7 +14,7 @@ import sbt.librarymanagement.{
  *  Dotty in .travis.yml.
  */
 object DottySupport {
-  val currentDottyRelease = "0.21.0-RC1" // TASTy version 18
+  val currentDottyRelease = "0.21.0" // TASTy version 18
   val dottyLibrary = "ch.epfl.lamp" % "dotty-library_0.21" % currentDottyRelease
   val dottyCompiler = "ch.epfl.lamp" % "dotty-compiler_0.21" % currentDottyRelease
   val dottyVersion = "0.21.0-RC1"
